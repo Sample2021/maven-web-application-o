da6f1323d2f84919a5dcd3910e5c3e41bcd25a60
charyveer75 <49182903+charyveer75@users.noreply.github.com>
2020-05-22 11:51:40 +0530

    Update HelloWorldController.java

--- src/main/java/com/mt/HelloWorldController.java
+++ src/main/java/com/mt/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.mt.services;
+package com.prodapt.services;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,11 +22,12 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Name", "Prodapt Solutions Pvt Ltd");
+		js.put("Calling Name", "Prodapt");
+		js.put("Founded", "29 January 1999"):
+		js.put("Headquaters", "Chennai");
+		js.put("Industry", "Software");
+		js.put("Parent", "The Jhaver Group");
 
 		return js.toString();
 }
